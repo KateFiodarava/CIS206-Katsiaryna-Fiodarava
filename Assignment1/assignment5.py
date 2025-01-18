afa954c8a4887042a89708dafe8ce7e3d748ab1a
Kate Fiodarava <katefiodarava@Kates-MacBook-Air.local>
2025-01-18 11:46:14 -0600

    first commit

---
+++ Assignment1/assignment5.py
@@ -0,0 +1,9 @@
+
+string_value = "My name is Kate!"
+float_value = 32.9
+integer_value = 5
+
+
+print(f"The type of '{string_value}' is: {type(string_value)}")
+print(f"The type of {float_value} is: {type(float_value)}")
+print(f"The type of {integer_value} is: {type(integer_value)}")
