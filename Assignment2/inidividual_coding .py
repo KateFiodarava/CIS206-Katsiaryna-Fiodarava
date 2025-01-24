215e5d000a225b3998f26c1a2aa4673e6743f7c7
Kate Fiodarava <katefiodarava@Kates-MacBook-Air.local>
2025-01-24 15:38:39 -0600

    Added new feature

---
+++ Assignment2/inidividual_coding .py
@@ -0,0 +1,25 @@
+
+# BMI 
+
+weight = float(input("Enter your weight in pound: "))
+height_feet = float(input("Enter your height in feet: "))
+height_inches = float(input("Enter your height in inches:"))
+
+total_height_inches = (height_feet * 12) + height_inches  
+bmi = ( weight * 703 ) / (total_height_inches ** 2)
+    
+
+if bmi < 18.5:
+    category = "underweight"
+elif 18.5 <= bmi <= 24.9:
+    category = "normal weight"
+elif 25 <= bmi <= 29.9:
+    category = "overweight"
+else:
+    category = "obesity"
+
+
+print(f"Your BMI is: {bmi:.2f}")
+print(f"This is {category}")
+
+
